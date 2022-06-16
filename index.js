9b8deab7a44fe128ce8b6b03f71bf0dc601e3dcc
Florjan96 <florianshiqerukaj@yahoo.com>
2022-06-16 23:42:06 +0200

    updates

--- index.js
+++ index.js
@@ -18,10 +18,10 @@
 // - The user id creates an identifiable relationship with the todos
 // - Sometimes it's good to map some values into a new data structure
 let users=[
-{id:1,Name:"John Bla",City:"Toronto"},
-{id:2,Name:"Anna Mo",City:"Tirane"},
-{id:3,Name:"Jane Turner",City:"Durres"},
-{id:4,Name:"Grent Jack",City:"Lyon"}
+{id:1,Name:"John Bla",City:"Toronto",tittle:"Lorem ipsum dolor sit amet"},
+{id:2,Name:"Anna Mo",City:"Tirane",tittle:"Lorem ipsum dolor sit amet"},
+{id:3,Name:"Jane Turner",City:"Durres",tittle:"Lorem ipsum dolor sit amet"},
+{id:4,Name:"Grent Jack",City:"Lyon",tittle:"Lorem ipsum dolor sit amet"}
 
 ]
 
@@ -33,15 +33,22 @@ for(let user of users){
 }
 
 
-let prove=prompt("What's your id?")
 
-for (prove of users ){
+// Prompt the user for a user id and then:
+// - Display an alert with the username and all the todos titles that belong to that user 
 
-    console.log(users[0])
+let promptedUserID = Number(prompt("Write  the user ID that you are choosing"));
+
+console.log(promptedUserID);
 
 
-
+let message1 = "";
+for(let user of users){
+  if (promptedUserID === users.id)
+  message1 = message1 + ("This is  username: " + user.Name )
 }
+
+
 
 
 
